ca0ff1cfc6e172ba607584110f41bcb8edf4499a
noelle none <noe.ililonga@gmail.com>
2018-06-06 08:07:03 -0400

    first commit

---
+++ Lab26a/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab26a.Startup))]
+namespace Lab26a
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
